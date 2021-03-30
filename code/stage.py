38d50373551751e70608e1c2e1e305228e7f71f1
Abram Hansen <hansenabram@gmail.com>
2021-03-29 18:34:56 -0600

    updated the stage class and the names of the tile image files

--- code/stage.py
+++ code/stage.py
@@ -13,64 +13,62 @@ class Stage:
         self._warriors = arcade.SpriteList()
         self._allSprites = arcade.SpriteList()
 
+        self._warriorTypes = []
+        self._dragonTypes = []
+
         self._stageData = json.load(open(jsonPath))
 
         self._setUp()
     
-    def getTiles(self):
+    def getTileSprites(self):
         return self._tiles
     
-    def getEnemies(self):
+    def getDragonSprites(self):
         return self._enemies
     
-    def getWarriors(self):
+    def getWarriorSprites(self):
         return self._warriors
     
     def getAllSprites(self):
         return self._allSprites
+
+    def getWarriorTypes(self):
+        return self._warriorTypes
+
+    def addWarrior(self,warrior):
+        self._warriors.append(warrior)
+        self._allSprites.append(warrior)
+
+    def removeWarrior(self,warrior):
+        warrior.remove_from_sprite_lists()
+
+    def addDragon(self,dragon):
+        self._enemies.append(dragon)
+        self._allSprites.append(dragon)
+
+    def removeDragon(self,dragon):
+        dragon.remove_from_sprite_lists()
+
+    def getCastleHealth(self):
+        return self._stageData.castleHealth
     
     def _setUp(self):
         self._setUpTiles(100, constants.SCREEN_HEIGHT - 100)
         self._setUpWarriors()
+        self._setUpDragons()
+
+    def _setUpDragons(self):
+        self._dragonTypes = self._stageData.enemyList
     
     def _setUpWarriors(self):
-        warrior = Warrior("images/warriors/1/0.png", 0.5)
-        warrior.center_x = 800
-        warrior.center_y = 100
-        self._warriors.append(warrior)
-        self._allSprites.append(warrior)
-        # for warriorNum in self._stageData["warriors"]:
-        #     warrior = 0
-        #     if warriorNum == 1:
-        #         warrior = Warrior("images/warriors/1/0.png")
-        #     elif warriorNum == 2:
-        #         warrior = Warrior("images/warriors/2/0.png")
-        #     elif warriorNum == 3:
-        #         warrior = Warrior("images/warriors/3/0.png")
-
-        #     warrior.center_x = 800
-            
-
-        #     self._warriors.append(warrior)
+        self._warriorTypes = self._stageData.warriors
 
     def _setUpTiles(self, init_x, init_y):
         next_x = init_x
         next_y = init_y
         index = 0
         for tileNum in self._stageData["boardArray"]:
-            tile = 0
-            if tileNum == 0:
-                tile = Tile("images/tiles/normalTile.png", constants.SCALING)
-                tile.textures.append(arcade.load_texture("images/tiles/normalTile_h.png"))
-            elif tileNum == 1:
-                tile = Tile("images/tiles/dragonLairTile.png", constants.SCALING)
-                tile.textures.append(arcade.load_texture("images/tiles/dragonLairTile_h.png"))
-            elif tileNum == 2:
-                tile = Tile("images/tiles/pathTile.png", constants.SCALING)
-                tile.textures.append(arcade.load_texture("images/tiles/pathTile_h.png"))
-            elif tileNum == 3:
-                tile = Tile("images/tiles/castleTile.png", constants.SCALING)
-                tile.textures.append(arcade.load_texture("images/tiles/castleTile_h.png"))
+            tile = Tile(tileNum)
 
             tile.center_x = next_x
             tile.center_y = next_y
