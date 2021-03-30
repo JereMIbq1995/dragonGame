df27eff6a1e177e5f95d62172631fc1dd7633f5f
JereMIbq1995 <duo13001@byui.edu>
2021-03-30 17:48:05 -0600

    fixed event handler mousepress bug

--- code/eventHandler.py
+++ code/eventHandler.py
@@ -1,12 +1,16 @@
+from code.warrior import Warrior
 class EventHandler:
     def __init__(self):
         self._warriorSelected = None
+        self._selectingMode = True    # True: selecting; False: placing
     
     def handleWarriorSelection(self, hud, mouse_x, mouse_y, stage):
         if (self._warriorSelected == None):
             self._warriorSelected = hud.getWarrior(mouse_x, mouse_y)
             if (not self._warriorSelected == None):
+                self._warriorSelected.setBeingSelected(True)
                 stage.addWarrior(self._warriorSelected)
+            self._selectingMode = False
 
     def handleWarriorPlacement(self, tiles, hud, mouse_x, mouse_y):
         if (not self._warriorSelected == None):
@@ -16,11 +20,13 @@ class EventHandler:
                     self._warriorSelected.center_x = tile.center_x
                     self._warriorSelected.center_y = tile.center_y
                     tile.setOccupied(True)
+                    self._warriorSelected.setBeingSelected(False)
                     warriorSet = True
                     break
             if (not warriorSet):
                 self._warriorSelected.remove_from_sprite_lists()
             self._warriorSelected = None
+            self._selectingMode = True
                 
 
     def handleHighlightingSprites(self, allSprites, mouse_x, mouse_y):
@@ -31,6 +37,10 @@ class EventHandler:
                 sprite.normalMode()
 
     def handleStickingWarriorToMouse(self, mouse_x, mouse_y):
+        print(self._warriorSelected)
         if (not self._warriorSelected == None):
             self._warriorSelected.center_x = mouse_x
             self._warriorSelected.center_y = mouse_y
+    
+    def getSelectingMode(self):
+        return self._selectingMode
