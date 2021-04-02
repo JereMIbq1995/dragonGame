7d744f44fbaeae3260cb685732fcfb92f165e52b
JereMIbq1995 <duo13001@byui.edu>
2021-04-02 00:09:22 -0600

    Dragon class test version

--- code/attacker.py
+++ code/attacker.py
@@ -2,7 +2,7 @@ import arcade
 class Attacker(arcade.Sprite):
     def __init__(self, path, scaling):
         self._health = 0.0
-        self._atk = 0.0
+        self._atk = 1.0
         self._attackRange = 0.0
         self._isAlive = bool
 
@@ -35,6 +35,13 @@ class Attacker(arcade.Sprite):
     def setAttackRange(self, attackRange):
         self.attackRange = attackRange
 
+    def setLocation(self, x, y):
+        super()._set_center_x(x)
+        super()._set_center_y(y)
+    
+    def setVelocity(self, x, y):
+        super()._set_change_x(x)
+        super()._set_change_y(y)
 
 class Warrior(Attacker):
     def __init__(self, typeId, beingSelected, scaling):
@@ -66,8 +73,11 @@ class Warrior(Attacker):
     (More functions and attributes need to be added to complete this class)
 """
 class Dragon(Attacker):
-    def __init__(self, typeId, scaling):
+    def __init__(self, typeId, absoluteVelocity, movingPath, scaling = 0.5):
         self._dragonTypeId = typeId
+        self._movingPath = movingPath
+        self._currentPathIndex = 0
+        self._absoluteVelocity = absoluteVelocity
         super().__init__(f"images/dragons/{typeId}/0.png", scaling)
 
     def getDragonTypeId(self):
@@ -75,3 +85,29 @@ class Dragon(Attacker):
 
     def setDragonTypeId(self, typeId):
         self._dragonTypeId = typeId
+
+    def getPath(self):
+        return self._path
+    
+    def setPath(self, path):
+        self._path = path
+    
+    def getAbsoluteVelocity(self):
+        return self._absoluteVelocity
+    
+    def setAbsoluteVelocity(self, absoluteVelocity):
+        self._absoluteVelocity = absoluteVelocity
+
+    def getCurrentDestTileIndex(self):
+        return self._movingPath[self._currentPathIndex]
+
+    def popCurrentDestTileIndex(self):
+        if (self._currentPathIndex != len(self._movingPath) - 1):
+            self._currentPathIndex += 1
+
+    def setVelocityByDestTile(self, tile):
+        distanceToLocation = arcade.get_distance_between_sprites(self, tile)
+        scaler = self._absoluteVelocity / distanceToLocation
+        change_x = scaler * ( tile.center_x - self.center_x )
+        change_y = scaler * ( tile.center_y - self.center_y )
+        super().setVelocity(change_x, change_y)
